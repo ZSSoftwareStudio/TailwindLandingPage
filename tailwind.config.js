f5d2f8a5ab7dc7f012902b9cf6e62f033eb78e96
Zarifprogrammer <zarifprogrammer@gmail.com>
2022-04-17 19:23:40 +0600

    Part 3

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: '"Oswald", sans-serif !important',
+      },
+    },
+  },
+  plugins: [],
+};
